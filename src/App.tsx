46b78cb5cfbfb86d6c1531a5de9ed6f4d83c5a67
b87y6cmbdy-lab <b87y6cmbdy@privaterelay.appleid.com>
2026-04-26 14:44:06 -0500

    fix: Remove redundant decorative divider SVG
    The SVG element acting as a decorative divider at the bottom of the application was found to be redundant and no longer served a visual purpose. This change removes the SVG to simplify the markup.

--- src/App.tsx
+++ src/App.tsx
@@ -164,13 +164,6 @@ const Hero = () => {
         </motion.div>
       </div>
     </div>
-
-    {/* Decorative divider */}
-      <div className="absolute bottom-0 left-0 w-full overflow-hidden leading-[0]">
-        <svg viewBox="0 0 1200 120" preserveAspectRatio="none" className="relative block w-full h-12 md:h-20 fill-bg">
-          <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V120H0V95.8C58,118.26,172.16,118.06,231.39,112.14,273,107.95,296,87.05,321.39,56.44Z"></path>
-        </svg>
-      </div>
     </section>
   );
 };
